7482678faccd9b3b2f09c626ba8d19a89231a072
santanu619 <mohapatra.santanu123@gmail.com>
2021-04-30 12:53:42 +0530

    Merged: Code for UserRegistrationDemo

---
+++ RegexUserRegistration/Pattern.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexUserRegistration
+{
+    public class Pattern
+    {
+        private const string FIRST_NAME_REGEX = "^[A-Z][a-zA-Z]{2,}$";
+        private const string LAST_NAME_REGEX = "^[A-Z][a-zA-Z]{2,}$";
+        private const string EMAIL_REGEX = "^[a-zA-Z]{3}[.]([a-zA-Z]{3})[@][a-zA-Z]{2}[.][a-zA-Z]{2}[.]([a-zA-Z]{2})$";
+        private const string MOBILE_REGEX = "^[1-9]{2,2}[][0-9]{10}$";
+        private const string PASSWORD_REGEX = "(?=.*[0-9])(?=.*[a-z])(?=.*[!@#$%^&*()_+]).{8,}$";
+
+        public bool firstNameCheck(string input)
+        {
+            return Regex.IsMatch(input, FIRST_NAME_REGEX);
+        }
+
+        public bool lastNameCheck(string input)
+        {
+            return Regex.IsMatch(input, LAST_NAME_REGEX);
+        }
+
+        public bool EmailCheck(string input)
+        {
+            return Regex.IsMatch(input, EMAIL_REGEX);
+        }
+
+        public bool MobileCheck(string input)
+        {
+            return Regex.IsMatch(input, MOBILE_REGEX);
+        }
+
+        public bool PasswordCheck(string input)
+        {
+            return Regex.IsMatch(input, PASSWORD_REGEX);
+        }
+
+    }
+}
--- RegexUserRegistration/Program.cs
+++ RegexUserRegistration/Program.cs
@@ -2,24 +2,22 @@
 
 namespace RegexUserRegistration
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            UC1_ValidFirstName ucfirstname = new UC1_ValidFirstName();
-            UC2_ValidLastName uclastname = new UC2_ValidLastName();
-            UC3_ValidEmail ucemail = new UC3_ValidEmail();
-            UC4_ValidPassword ucpassword = new UC4_ValidPassword();
-            bool result1 = ucfirstname.validateFirstName("Santanu");
-            bool result2 = uclastname.validateLastName("Mohapatra");
-            bool result3 = ucemail.validateEmail("mohapatra.santanu123@gmail.com");
-            bool result4 = ucpassword.validatePassword("Myfamily123");
-            Console.WriteLine(result1);
-            Console.WriteLine(result2);
-            Console.WriteLine(result3);
-            Console.WriteLine(result4);
-            Console.WriteLine("First Name is Valid!");
-            Console.WriteLine("Please Proceed");
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Welcome to the User Registration Cell");
+            bool firstName = pattern.firstNameCheck("Santanu");
+            Console.WriteLine("FirstName " + firstName);
+            bool lastName = pattern.lastNameCheck("Mohapatra");
+            Console.WriteLine("LastName " + lastName);
+            bool email = pattern.EmailCheck("mohapatra.santanu123@gmail.com");
+            Console.WriteLine("Email " + email);
+            bool mobile = pattern.MobileCheck(" 91 6294476694");
+            Console.WriteLine("Mobile " + mobile);
+            bool password = pattern.PasswordCheck("Myfamily@61923");
+            Console.WriteLine("Password " + password);
             Console.ReadKey();
         }
     }
