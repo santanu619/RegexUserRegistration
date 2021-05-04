ae5eca088beda94884a0da74315e8ef532742805
santanu619 <mohapatra.santanu123@gmail.com>
2021-05-04 14:11:09 +0530

    Added: Code of RegexUserRegistration using lambda expression

---
+++ RegexUserRegistrationLambdaExpression/Pattern.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexApp
+{
+    public class Pattern
+    {
+        const string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
+        const string REGEX_LASTNAME = "^[A-Z][a-z]{2,}$";
+        const string REGEX_EMAIL = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        const string REGEX_MOBILE = "^[0-9]{2}[ ][1-9][0-9]{9}$";
+        const string REGEX_PASSWORD = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$";
+
+        public Func<string, bool> firstNameCheck = (input) =>
+        { return Regex.IsMatch(input, REGEX_FIRSTNAME); };
+        public Func<string, bool> lastNameCheck = (input) =>
+        { return Regex.IsMatch(input, REGEX_LASTNAME); };
+        public Func<string, bool> EmailCheck = (input) =>
+        { return Regex.IsMatch(input, REGEX_EMAIL); };
+        public Func<string, bool> MobileCheck = (input) =>
+        { return Regex.IsMatch(input, REGEX_MOBILE); };
+        public Func<string, bool> PasswordCheck = (input) =>
+        { return Regex.IsMatch(input, REGEX_PASSWORD); };
+
+    }
+}
---
+++ RegexUserRegistrationLambdaExpression/Program.cs
@@ -0,0 +1,25 @@
+﻿using RegexApp;
+using System;
+
+namespace RegexUserRegistrationLambdaExpression
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Welcome to the User Registration Cell");
+            bool firstName = pattern.firstNameCheck("Santanu");
+            Console.WriteLine("FirstName " + firstName);
+            bool lastName = pattern.lastNameCheck("Mohapatra");
+            Console.WriteLine("LastName " + lastName);
+            bool email = pattern.EmailCheck("mohapatra.santanu123@gmail.com");
+            Console.WriteLine("Email " + email);
+            bool mobile = pattern.MobileCheck("91 6294476694");
+            Console.WriteLine("Mobile " + mobile);
+            bool password = pattern.PasswordCheck("Myfamily@61923");
+            Console.WriteLine("Password " + password);
+            Console.ReadKey();
+        }
+    }
+}
